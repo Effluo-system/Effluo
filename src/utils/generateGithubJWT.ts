50490b07f7b1a323f0c1cd372a5ae0254ac75b7f
Navojith <navojith22@gmail.com>
2025-03-18 10:20:21 +0530

    fix: jwt bug

--- src/utils/generateGithubJWT.ts
+++ src/utils/generateGithubJWT.ts
@@ -10,3 +10,16 @@ export const jwtToken = jwt.sign(
   env.privateKey,
   { algorithm: 'RS256' }
 );
+
+export const createNewJWT = () => {
+  const token = jwt.sign(
+    {
+      iss: env.appId,
+      iat: Date.now() / 1000,
+      exp: Math.floor(Date.now() / 1000) + 60 * 10,
+    },
+    env.privateKey,
+    { algorithm: 'RS256' }
+  );
+  return token;
+};
